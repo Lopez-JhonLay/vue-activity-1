95987b94f4c81326f0c2e9721d2df98e1de2921d
Jhon Lay Lopez <132878493+Lopez-JhonLay@users.noreply.github.com>
2025-03-24 13:26:09 +0800

    create registration and login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import type { FormInstance, FormRules } from "element-plus";
+import { Lock, User, Loading } from "@element-plus/icons-vue";
+
+const ruleFormRef = ref<FormInstance>();
+
+const validateUsername = (rule: any, value: any, callback: any) => {
+  if (value === "") {
+    callback(new Error("Please input the username"));
+  } else {
+    callback();
+  }
+};
+
+const validatePass = (rule: any, value: any, callback: any) => {
+  if (value === "") {
+    callback(new Error("Please input the password"));
+  } else {
+    callback();
+  }
+};
+
+const ruleForm = reactive({
+  username: "",
+  pass: "",
+});
+
+const rules = reactive<FormRules<typeof ruleForm>>({
+  username: [{ validator: validateUsername, trigger: "blur" }],
+  pass: [{ validator: validatePass, trigger: "blur" }],
+});
+
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log("submit!", ruleForm.username);
+    } else {
+      console.log("error submit!");
+    }
+  });
+};
+
+const isLoading = ref(false);
+</script>
+
+<template>
+  <el-form
+    ref="ruleFormRef"
+    style="max-width: 400px"
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    label-width="auto"
+    class="demo-ruleForm"
+  >
+    <div>
+      <h1>Login</h1>
+    </div>
+    <el-form-item prop="username">
+      <el-input
+        :prefix-icon="User"
+        v-model="ruleForm.username"
+        placeholder="Username"
+      />
+    </el-form-item>
+    <el-form-item prop="pass">
+      <el-input
+        :prefix-icon="Lock"
+        v-model="ruleForm.pass"
+        type="password"
+        autocomplete="off"
+        placeholder="Password"
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-button v-if="!isLoading" @click="submitForm(ruleFormRef)">
+        LOGIN
+      </el-button>
+      <el-button v-else :icon="Loading" loading />
+    </el-form-item>
+  </el-form>
+</template>
+
+<style scoped>
+h1 {
+  color: white;
+}
+
+.demo-ruleForm {
+  width: 100%;
+  margin-top: -200px;
+  margin-left: -20px;
+}
+
+:deep(.el-input__wrapper) {
+  background-color: transparent;
+}
+
+:deep(.el-input__inner) {
+  color: white;
+}
+
+:deep(.el-button) {
+  width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,5 @@
-<template></template>
+<template>
+  <h1>Home</h1>
+  <router-link to="/login">Login</router-link>
+  <router-link to="/register">Register</router-link>
+</template>
